798a7842bbfae95e3545e7ce7437d6f54ba6a50e
krlvn <92533330+krlvn@users.noreply.github.com>
2022-11-02 13:44:18 +0200

    Рефакторинг моделей

--- app/api/models.py
+++ app/api/models.py
@@ -2,29 +2,41 @@ from django.contrib.gis.db import models
 
 
 class City(models.Model):
-    address = models.CharField(max_length=500)
-    postal_code = models.PositiveIntegerField(blank=True, null=True)
-    country = models.CharField(max_length=120, blank=True)
-    federal_district = models.CharField(max_length=20, blank=True)
-    region_type = models.CharField(max_length=10, blank=True)
-    region = models.CharField(max_length=120, blank=True)
-    area_type = models.CharField(max_length=10, blank=True)
-    area = models.CharField(max_length=120, blank=True)
-    city_type = models.CharField(max_length=10, blank=True)
-    city = models.CharField(max_length=120, blank=True)
-    settlement_type = models.CharField(max_length=10, blank=True)
-    settlement = models.CharField(max_length=120, blank=True)
-    kladr_id = models.PositiveBigIntegerField(blank=True, null=True)
-    fias_id = models.CharField(max_length=36, blank=True)
-    fias_level = models.PositiveSmallIntegerField(blank=True, null=True)
-    capital_marker = models.PositiveSmallIntegerField(blank=True, null=True)
-    okato = models.PositiveBigIntegerField(blank=True, null=True)
-    oktmo = models.PositiveBigIntegerField(blank=True, null=True)
-    tax_office = models.PositiveSmallIntegerField(blank=True, null=True)
-    timezone = models.CharField(max_length=50, blank=True)
-    geo = models.PointField(unique=True)
-    population = models.CharField(max_length=10, blank=True)
-    foundation_year = models.CharField(max_length=20, blank=True)
+    address = models.CharField(max_length=500, verbose_name='Address')
+    postal_code = models.PositiveIntegerField(blank=True, null=True, verbose_name='Postal Code')
+    country = models.CharField(max_length=120, blank=True, verbose_name='Country')
+    federal_district = models.CharField(max_length=20, blank=True, verbose_name='Federal District')
+    region_type = models.CharField(max_length=10, blank=True, verbose_name='Region Type')
+    region = models.CharField(max_length=120, blank=True, verbose_name='Region')
+    area_type = models.CharField(max_length=10, blank=True, verbose_name='Area Type')
+    area = models.CharField(max_length=120, blank=True, verbose_name='Area')
+    city_type = models.CharField(max_length=10, blank=True, verbose_name='City Type')
+    city = models.CharField(max_length=120, blank=True, verbose_name='City')
+    settlement_type = models.CharField(max_length=10, blank=True, verbose_name='Settlement Type')
+    settlement = models.CharField(max_length=120, blank=True, verbose_name='Settlement')
+    kladr_id = models.PositiveBigIntegerField(blank=True, null=True, verbose_name='Kladr ID')
+    fias_id = models.CharField(max_length=36, blank=True, verbose_name='Fias ID')
+    fias_level = models.PositiveSmallIntegerField(blank=True, null=True, verbose_name='Fias Level')
+    capital_marker = models.PositiveSmallIntegerField(blank=True, null=True, verbose_name='Capital')
+    okato = models.PositiveBigIntegerField(blank=True, null=True, verbose_name='OKATO')
+    oktmo = models.PositiveBigIntegerField(blank=True, null=True, verbose_name='OKTMO')
+    tax_office = models.PositiveSmallIntegerField(blank=True, null=True, verbose_name='Tax Office')
+    timezone = models.CharField(max_length=50, blank=True, verbose_name='Timezone')
+    geo = models.PointField(unique=True, verbose_name='Geo')
+    population = models.CharField(max_length=10, blank=True, verbose_name='Population')
+    foundation_year = models.CharField(max_length=20, blank=True, verbose_name='Doundation Year')
+    create_date = models.DateTimeField(auto_now_add=True, verbose_name='Date of creating', editable=False)
+    update_date = models.DateTimeField(auto_now=True, verbose_name='Date of editing', editable=False)
+    deleted = models.BooleanField(default=False)
 
-    class Meta:
-        db_table = 'cities'
+    class Meta():
+        verbose_name = 'City'
+        verbose_name_plural = 'Cities'
+        ordering = ('address',)
+
+    def __str__(self):
+        return f'{self.pk} {self.address}'
+
+    def delete(self, *args):
+        self.deleted = True
+        self.save()
